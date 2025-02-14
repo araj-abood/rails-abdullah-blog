a4df793b9417c6e16103be36fbced73890e0c7e2
Abdullah Al-araj <alaraj.abdullah02@gmail.com>
2025-02-14 18:12:40 +0300

    First comit

---
+++ app/controllers/article_controller.rb
@@ -0,0 +1,46 @@
+class ArticleController < ApplicationController
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
+  def index
+    @articles = Article.all
+  end
+  def show
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def edit
+  end
+
+  def update
+    if @article.update article_params
+      flash[:noteice] = "Article was updated successfully"
+      redirect_to article_path(@article)
+    else
+      render 'edit'
+    end
+  end
+
+  def create
+    @article = Article.new article_params
+    if @article.save
+      flash[:noteice] = "Article was created successfully"
+      redirect_to @article
+    else
+      render "new"
+    end
+  end
+  def destroy
+    @article.destroy
+    flash[:notice] = "Article was deleted successfully"
+    redirect_to article_index_path
+  end
+  private
+  def set_article
+    @article = Article.find(params[:id])
+  end
+  def article_params
+    params.require(:article).permit([:title, :desc])
+  end
+end
