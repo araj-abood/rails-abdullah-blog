4402035418671c848e4815a225ba197658d65fc6
Abdullah Al-araj <alaraj.abdullah02@gmail.com>
2025-02-17 19:11:06 +0300

    Add users table and associate with articles

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,15 @@
 class User < ApplicationRecord
+  before_save { self.email = email.downcase }
 
+  has_many :articles
+  validates :username, presence: true,
+            uniqueness: { case_sensitive: false },
+            length: { minimum: 3, maximum: 25 }
+  EMAIL_REGEX = /\A[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}\Z/i
+  validates :email, presence: true,
+            uniqueness: { case_sensitive: false },
+            length: { maximum: 105 },
+            format: { with: EMAIL_REGEX }
+
+  has_secure_password
 end
