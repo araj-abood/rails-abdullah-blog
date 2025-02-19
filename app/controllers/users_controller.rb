02d551b87f82a51b4de9d5c7b2f75fc8e72c3a02
Abdullah Al-araj <alaraj.abdullah02@gmail.com>
2025-02-19 19:30:33 +0300

    Add edit user page, to allow userss to change their username or password, or email

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,6 @@ class UsersController < ApplicationController
   end
 
   def create
-
     @user = User.new(user_params)
     if @user.save
       flash[:notice] = "Welcome to the alpha Blog. #{@user.username}"
@@ -13,6 +12,22 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice] = "Account updated Successfully."
+      redirect_to articles_path
+    else
+      render 'edit'
+    end
+  end
+
+
   private
   def user_params
     params.require(:user).permit(:username, :password, :email)
