8bb7135e5bc5fd370ae0535f5b050834863ecdd2
Abdullah Al-araj <alaraj.abdullah02@gmail.com>
2025-02-27 10:21:18 +0300

    Complete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,29 @@
 class UsersController < ApplicationController
+
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :require_user, only: [:edit, :update]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
+
   def new
-    @user = User.new
+    if logged_in?
+      redirect_to user_path(current_user)
+    else
+      @user = User.new
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def index
+    @users = User.paginate(page: params[:page], per_page: 5)
   end
 
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:notice] = "Welcome to the alpha Blog. #{@user.username}"
       redirect_to articles_path
     else
@@ -21,15 +39,34 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:notice] = "Account updated Successfully."
-      redirect_to articles_path
+      redirect_to user_path(@user)
     else
       render 'edit'
     end
   end
 
+  def destroy
+    if @user.destroy
+      flash[:notice] = "Account and all associsated articles deleted Successfully."
+      session[:user_id] = nil if @user == current_user
+      redirect_to articles_path
+    end
+  end
 
   private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
   def user_params
     params.require(:user).permit(:username, :password, :email)
+  end
+
+  def require_same_user
+    if current_user != @user && !current_user.is_admin?
+      flash[:alert] = "You can only edit or delete your own account"
+      redirect_to user_path(current_user)
+    end
   end
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,6 @@
 class Article < ApplicationRecord
   belongs_to :user
-  validates :title, presence: true, length: {minimum: 5, maximum: 50}
-  validates :desc, presence: true, length: {minimum: 5, maximum: 500}
+
+  validates :title, presence: true, length: { minimum: 5, maximum: 50 }
+  validates :desc, presence: true, length: { minimum: 5, maximum: 500 }
 end
